f14007315244be5e7e86aa58c1cc49c775373897
tatiana <khen.tatiana@gmail.com>
2020-03-08 15:25:43 -0700

    submitting assignments for 1_16 - 1_17

---
+++ Coursera_Python/1_16_Assignment: 100 раз подряд в квадрате.py
@@ -0,0 +1,3 @@
+n = str(input())
+n_hundred = (int(n * 100) ** 2)
+print(n_hundred)
---
+++ Coursera_Python/1_17_Assignment: МКАД.py
@@ -0,0 +1,11 @@
+v = int(input())
+t = int(input())
+
+if(t > 0):
+    if(-1000 < v < 0):
+        dist = 109 - ((-1 * v * t) % 109)
+        print(dist)
+
+    if (0 < v < 1000):
+        dist = (v * t) % 109
+        print(dist)
