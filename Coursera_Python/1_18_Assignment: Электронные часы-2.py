8d3fcc5edf703cf21d691a95132edbb75e4d117c
tatiana <khen.tatiana@gmail.com>
2020-03-08 17:22:41 -0700

    submitting assignments for 1_17 - 1_18

--- Coursera_Python/1_18_Assignment: Электронные часы-2.py
+++ Coursera_Python/1_18_Assignment: Электронные часы-2.py
@@ -1,5 +1,7 @@
-n = int(input())
-if 0 <= n < (10 ** 7):
-    hours = n % (60 * 24) // 60
-    min = n % 60
-    print(hours, min)
+N = int(input())
+n = N % (3600 * 24)
+if n > 0:
+    hours = n // 3600
+    min = n % 3600 // 60
+    sec = n % 60
+    print(hours,':', min // 10, ':', sec // 10, sec % 10, sep='')
